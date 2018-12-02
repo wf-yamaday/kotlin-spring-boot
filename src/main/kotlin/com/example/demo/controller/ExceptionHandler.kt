9e4f87c871460c7f74c28f9968dac7a222ac98f3
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2018-12-03 08:41:05 +0900

    エラー時のレスポンス整形クラスの作成

---
+++ src/main/kotlin/com/example/demo/controller/ExceptionHandler.kt
@@ -0,0 +1,8 @@
+package com.example.demo.controller
+
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+// controllerの例外処理を行うクラス
+@RestControllerAdvice
+class ExceptionHandler {
+}
