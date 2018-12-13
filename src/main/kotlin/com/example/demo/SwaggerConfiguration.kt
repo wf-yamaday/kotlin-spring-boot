1941fb39fb45f214291317b7f1ce5e0b0e907aea
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2018-12-13 13:10:48 +0900

    swaggerの導入

---
+++ src/main/kotlin/com/example/demo/SwaggerConfiguration.kt
@@ -0,0 +1,34 @@
+package com.example.demo
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfiguration {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .groupName("api")
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
+            .build()
+            .apiInfo(apiInfo())
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+            .title(" REST API List")
+            .description("REST APIの一覧")
+            .version("1.0")
+            .contact(Contact("yamaday", "https://github/wf-yamaday", "e155417k@st.u-gakugei.ac.jp"))
+            .build()
+    }
+}
--- src/main/kotlin/com/example/demo/controller/PostController.kt
+++ src/main/kotlin/com/example/demo/controller/PostController.kt
@@ -1,10 +1,15 @@
 package com.example.demo.controller
 
+import com.example.demo.dto.NotFoundException
 import com.example.demo.entity.Post
 import com.example.demo.service.PostService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 import java.util.Optional
 
 @RestController
@@ -17,12 +22,10 @@ class PostController(
         return ResponseEntity.ok(postService.findAll())
     }
     @GetMapping("/{id}")
-    @ResponseStatus(value= HttpStatus.NOT_FOUND)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
     fun getPost(@PathVariable id: Int): ResponseEntity<Optional<Post>> {
         return if (postService.findById(id).isPresent) {
             ResponseEntity.ok().body(postService.findById(id))
-        } else {
-            ResponseEntity.notFound().build()
-        }
+        } else throw NotFoundException("not found")
     }
 }
