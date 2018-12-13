413209e1ee0457e3097fb62be5a0af0ada9f99cc
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2018-12-13 16:40:04 +0900

    関連の追加

--- src/main/kotlin/com/example/demo/entity/Post.kt
+++ src/main/kotlin/com/example/demo/entity/Post.kt
@@ -1,5 +1,6 @@
 package com.example.demo.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import java.io.Serializable
 import java.time.LocalDateTime
 import javax.persistence.Entity
@@ -7,6 +8,9 @@ import javax.persistence.Table
 import javax.persistence.Id
 import javax.persistence.GeneratedValue
 import javax.persistence.Column
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 import javax.persistence.PrePersist
 import javax.persistence.PreUpdate
 
@@ -21,7 +25,11 @@ data class Post(
     @Column(name = "body", nullable = true)
     var body: String,
     @Column(name = "updated", nullable = false)
-    var updated: LocalDateTime = LocalDateTime.now()
+    var updated: LocalDateTime = LocalDateTime.now(),
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, insertable = false, updatable = false, name = "boards_id")
+    var board: Board
 ) : Serializable {
     @PrePersist
     fun prePersist() {
